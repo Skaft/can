45cde06df312ff6a75c4306ea70d196003e594ff
Skaft <ragnar.gerholm@gmail.com>
2021-05-15 10:04:24 +0200

    add bit stuffing function

--- helpers.py
+++ helpers.py
@@ -1,5 +1,23 @@
+from itertools import groupby
+
+
 def int_to_bitsequence(value, length=None):
     bits = format(value, 'b')
     if length:
         bits = bits.zfill(length)
     return [int(b) for b in bits]
+
+
+def pad_sequence(intlist, max_width=5):
+    if intlist == []:
+        return []
+    digit, group = next(groupby(intlist))
+    subsequence = list(group)
+    cutoff = min(len(subsequence), max_width)
+    head = intlist[:cutoff]
+    if cutoff == max_width:
+        opposite = 1 - digit
+        tail = [opposite] + intlist[cutoff:]
+    else:
+        tail = intlist[cutoff:]
+    return head + pad_sequence(tail)    
---
+++ test_helpers.py
@@ -0,0 +1,27 @@
+from helpers import pad_sequence
+
+
+def test_simple_bit_stuffing():
+    bits = [1,1,1,1,1,1,1]
+    expected = [1,1,1,1,1,0,1,1]
+    assert pad_sequence(bits) == expected
+
+def test_many_bit_stuffings():
+    bits = [
+        1,0,1,1,1,1,1,
+        1,0,1,1,1,1,1,
+        1,0,0,0,0,0,0,
+        1,0,1,1,1,1,1,
+    ]
+    expected = [
+        1,0,1,1,1,1,1,0,
+        1,0,1,1,1,1,1,0,
+        1,0,0,0,0,0,1,0,
+        1,0,1,1,1,1,1,0,
+    ]
+    assert pad_sequence(bits) == expected
+
+def test_connected_long_regions():
+    bits = [0,0,0,0,0,1,1,1,1]
+    expected = [0,0,0,0,0,1,1,1,1,1,0]
+    assert pad_sequence(bits) == expected
